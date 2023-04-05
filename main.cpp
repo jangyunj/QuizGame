e717304af355be68bbb74bf78e5f7626c775555b
Yun Jang <jang.yunj7@gmail.com>
2023-04-04 23:46:09 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string questions[] = {"1. What year was C++ created?: ",
+                               "2. Who invented C++?: ",
+                               "3. What is the precedessor of C++?: ",
+                               "4. Is the earth flat?: "};
+
+    std::string options[][4] = {{"A. 1969", "B. 1975", "C. 1985", "D. 1989"},
+                                {"A. Guido van Rossum", "B. Bjarne Stroustrup", "C. John Carmack", "D. Mark Zukerburg"},
+                                {"A. C", "B. C+", "C. C--", "D. B++"},
+                                {"A. Yes", "B. No", "C. Maybe", "D. Sometimes"}};
+
+    char answerKey[] = {'C', 'B', 'A', 'B'};
+
+    int size = sizeof(questions) / sizeof(questions[0]);
+    char guess;
+    int score;
+
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << "******************\n";
+        std::cout << questions[i] << '\n';
+        std::cout << "******************\n";
+
+        for (int j = 0; j < sizeof(options[i]) / sizeof(options[i][0]); j++)
+        {
+            std::cout << options[i][j] << '\n';
+        }
+    }
+}
